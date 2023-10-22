ed7f4ff90d2be696918df918934c80b02627e32f
HRLBZE <1214899196@qq.com>
2023-10-22 19:07:46 +0800

    10-22

--- C++_Primer_Plus/Source.cpp
+++ C++_Primer_Plus/Source.cpp
@@ -1,49 +1,123 @@
 #include<iostream>
-int main()
+#include<string>
+Try
 {
-	using namespace std;
-	int choose;
-	double oil;
-	double mile;
-	double kilo;
-	double o_m;
-	double k_o;
-	cout << "choose the method you want to use and the only number you can input are zero,one :____\b\b\b\b";
-	cin >> choose;
-
-	if (choose == 0)
-	{
-		cout << "Please input the oil and the mile" << endl;
-		cin >> oil;
-		cin >> mile;
-		o_m = mile / oil;
-		cout << "one oil have " << o_m << " miles" << endl;
-	}
-	if (choose == 1)
-	{
-		cout << "Please input the oil and the kilometer" << endl;
-		cin >> oil;
-		cin >> kilo;
-		k_o = oil / kilo;
-		k_o *= 100;
-		cout << "hundred kilo will spent " << k_o << " oil" << endl;
-		cout << "Do you want to know the USA's method?" << endl;
-		cout << "if yes please input one,else please input 0" << endl;
-		int der;
-		cin >> der;
-		if (der == 1)
-		{
-			double litre;
-			double mile;
-			double x;
-			litre = oil * 3.785;
-			mile = kilo * 62.14;
-			x = mile / litre;
-			cout << "The measure is " << x << endl;
-		}
-	}
-	return 0;
+	 
 }
+int main()
+//#include<stdio.h>
+//int main()
+//{
+//	while (0.3)
+//	{
+//		printf("hello");
+//	}
+//	return 0;
+//}
+//#include<iostream>
+//#include<cstring>
+//#include<string>
+//int main()
+//{
+//	using namespace std;
+//
+//	cout<<R"+*(nihao)*+";
+//	//using namespace std;
+//	//string a;
+//	//cin >> a;
+//	//cout << "the number of a has" << size(a) << endl;
+//	return 0;
+//}
+//int main()
+//{
+//	using namespace std;
+//	const int num = 20;
+//	char ch;
+//	char name1[num];
+//	char name2[num];
+//	char name3[num];
+//	cout << "output the number!" << endl;
+//	cin.get(name1, num).get(ch);
+//	cin.get(name2, num).get();
+//	cout << name1 << endl;
+//	cout << name2 << endl;
+//	cin.getline(name3, num);
+//	cout << name3 << endl;
+//	return 0;
+//}
+//int main()
+//{
+//	const int number = 15;
+//	using namespace std;
+//	char str[number];
+//	cout << "input the str you want to know _____\b\b\b\b\b";
+//	cin.getline(str, number);
+//	char text[number];
+//	cin.getline(text, number);
+//	cout << "the number is " << str << "   " << text << endl;
+//	return 0;
+//}
+//int main()
+//{
+//	using namespace std;
+//
+//	//using namespace std;
+//	//cout << "hello"  "wrold" << endl;
+//	//return 0;
+//}
+//int main()
+//{
+//	using namespace std;
+//	int A[3]{ 2,3,5 };
+//	cout << A << endl;
+// 
+//	return 0;
+//}
+//int main()
+//{
+//	using namespace std;
+//	int choose;
+//	double oil;
+//	double mile;
+//	double kilo;
+//	double o_m;
+//	double k_o;
+//	cout << "choose the method you want to use and the only number you can input are zero,one :____\b\b\b\b";
+//	cin >> choose;
+//
+//	if (choose == 0)
+//	{
+//		cout << "Please input the oil and the mile" << endl;
+//		cin >> oil;
+//		cin >> mile;
+//		o_m = mile / oil;
+//		cout << "one oil have " << o_m << " miles" << endl;
+//	}
+//	if (choose == 1)
+//	{
+//		cout << "Please input the oil and the kilometer" << endl;
+//		cin >> oil;
+//		cin >> kilo;
+//		k_o = oil / kilo;
+//		k_o *= 100;
+//		cout << "hundred kilo will spent " << k_o << " oil" << endl;
+//		cout << "Do you want to know the USA's method?" << endl;
+//		cout << "if yes please input one,else please input 0" << endl;
+//		int der;
+//		cin >> der;
+//		if (der == 1)
+//		{
+//			double litre;
+//			double mile;
+//			double x;
+//			litre = oil * 3.785;
+//			mile = kilo * 62.14;
+//			x = mile / litre;
+//			cout << "The measure is " << x << endl;
+//		}
+//	}
+//	return 0;
+//}
 //·ûºÅ³£Á¿	1.#define x 128
 //			2.ÀàËÆconst int x
 //int main()
